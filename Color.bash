ba26afaed40dabc7a2c0b43b024e46ad9595667f
Joshua-Hoffman-r <63210650+Joshua-Hoffman-r@users.noreply.github.com>
2020-05-02 16:01:36 -0700

    Add files via upload

---
+++ Color.bash
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+#    _________________________________________________________________________________________________
+#   |* ___________________________________________GPL-3_Licence_____________________________________ *|
+#   | /                                                                                             \ |
+#   | |         This program is free software: you can redistribute it and/or modify                | |
+#   | |         it under the terms of the GNU General Public License as published by                | |
+#   | |         the Free Software Foundation, either version 3 of the License, or                   | |
+#   | |         (at your option) any later version.                                                 | |
+#   | |                                                                                             | |
+#   | |         This program is distributed in the hope that it will be useful,                     | |
+#   | |         but WITHOUT ANY WARRANTY; without even the implied warranty of                      | |
+#   | |         MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                       | |
+#   | |         GNU General Public License for more details.                                        | |
+#   | |                                                                                             | |
+#   | |         You should have received a copy of the GNU General Public License                   | |
+#   | |         along with this program.  If not, see <http://www.gnu.org/licenses/>.               | |
+#   | |                                                                                             | |
+#   | \_____________________________________________________________________________________________/ |
+#   |* ___________________________________________Colors_5/2/2020_________________________________ *|
+#   | /                                                                                             \ |
+#   | |                                    Written by: Joshua Hoffman                               | |
+#   | |                                 joshua.hoffman.ray@protonmail.com                           | |
+#   | |                                                                                             | |
+#   | \________________________________________Modulation_Project___________________________________/ |
+#   |_________________________________________________________________________________________________|
+#
+
+
+Color () {
+
+declare -Ag Canvis Color
+Portfolio=( "$(echo "$(dirname "$(realpath $0)")/$0")" $@ )
+
+	for Item in ${Portfolio[@]}; do
+
+	Colors=(
+	[1]="$(echo ${Item%/*})"
+	[2]="$(cat $Item)"
+	[3]="$(wc -c $Item)"
+	[4]="$(wc -w $Item)"
+	[5]="$(wc -l $Item)" )
+
+	declare -Ag Color[{1..5}]=$(echo ${Color[$((A++))]} | sha256sum | awk '{print substr($1, 1, 32)}' )
+
+		for Rain in ${Color[@]}; do
+		export Shade=${Rain}${Shade}
+		export Purple=$((Purple+${#Rain}))
+		done
+
+	Blend=( $( seq 1 $Purple | sort -R ) )
+
+		for Stroke in ${Blend[@]} 0; do
+		Canvis[${Stroke}]=${Shade:${Stroke}:1}
+		unset Hue
+		done
+
+	done
+
+}
+
+Color $@
+
+echo Verfication:
+
+	for Paint in ${!Canvis[@]}; do
+	echo -n ${Canvis[$((Cover++))]}
+	done
+
+echo -e \\nRandomized:
+echo "${Canvis[@]}" | tr -d ' ' | wc -c
